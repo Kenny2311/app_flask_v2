fc5e3356c86a45fffc4f91e0ef9c7ae3e770ec7a
Kenny2311 <149954815+Kenny2311@users.noreply.github.com>
2026-04-15 19:02:25 -0500

    Conexión base de datos modificado para despliegue

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-super-secreta'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "clave-super-secreta")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
