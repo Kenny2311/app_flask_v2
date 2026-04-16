4738aca5fdecf4fd7b2eac6043d0d392f7d9ce36
Kenny2311 <149954815+Kenny2311@users.noreply.github.com>
2026-04-15 20:45:23 -0500

    Change DATABASE_URL to DATABASE_PUBLIC_URL
    Updated DATABASE_URL to use DATABASE_PUBLIC_URL and adjusted SQLALCHEMY_DATABASE_URI assignment.

--- config.py
+++ config.py
@@ -4,12 +4,11 @@ class Config:
 
     SECRET_KEY = os.environ.get("SECRET_KEY", "clave-super-secreta")
 
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL = os.environ.get("DATABASE_PUBLIC_URL")
 
-    if DATABASE_URL:
-        if DATABASE_URL.startswith("postgres://"):
-            DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL if DATABASE_URL else "sqlite:///local.db"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
